a660d07c1c02504d9f5cb1902ac7734c8410c997
DESKTOP-U6640EL\yuminho <dbalsgh1312@naver.com>
2023-12-06 08:50:18 +0900

    프로젝트 디렉토리구성

---
+++ Developer-Helper/Developer-Helper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Developer_Helper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Developer-Helper/Developer-HelperTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Developer_HelperTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
