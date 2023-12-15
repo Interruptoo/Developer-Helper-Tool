a3f1e381b3f86885f50f41e41b2d2f538816948a
DESKTOP-U6640EL\yuminho <dbalsgh1312@naver.com>
2023-12-15 16:01:53 +0900

    MenuItem Command 추가 DataContext 추가

---
+++ Developer-Helper/Developer-Helper/Model/TableInfo_Model.cs
@@ -0,0 +1,17 @@
+﻿using Developer_Helper.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer_Helper.Model
+{
+    class TableInfo_Model : ModelBase
+    {
+        public string TableName { get; }
+        public string Owner {  get; }
+        public string TableCommant { get; }
+                
+    }
+}
--- Developer-Helper/Developer-Helper/ViewModel/MainWindow_ViewModel.cs
+++ Developer-Helper/Developer-Helper/ViewModel/MainWindow_ViewModel.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfMvvm.Commands;
 
 namespace Developer_Helper.ViewModel
 {
     class MainWindow_ViewModel
     {
+        #region Const
+
+        #endregion Const
+
+        #region Member Variables
+
+        #endregion Member Variables
+
+        #region View Properties
+        
+        #endregion View Properties
+
+        #region Model Properties
+
+        #endregion Model Properties
+
+        #region Constructors
+
+        #endregion Constructors
+
+        #region Command
+        private ICommand _selectedMenuItem;
+        public ICommand SelectedMenuItem
+        {
+            get
+            {
+                if (_selectedMenuItem == null)
+                    _selectedMenuItem = new RelayCommand(p => SetVisibleUserControl());
+
+                return _selectedMenuItem;
+                                    
+            }
+
+        }
+
+        #endregion Command
+
+        #region Method
+        private void SetVisibleUserControl()
+        {
+
+        }
+        #endregion Method
     }
 }
