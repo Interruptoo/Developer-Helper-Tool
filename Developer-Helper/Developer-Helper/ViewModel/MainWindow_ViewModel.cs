8c879ddc4425b852142fe39ce776ba9ca82791c5
yuminho <dbalsgh1312@naver.com>
2023-12-18 23:57:25 +0900

    DBConnection 메뉴 선택 시 popup 호출기능 개발완료

--- Developer-Helper/Developer-Helper/ViewModel/MainWindow_ViewModel.cs
+++ Developer-Helper/Developer-Helper/ViewModel/MainWindow_ViewModel.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using WpfMvvm.Commands;
 
@@ -23,6 +24,9 @@ namespace Developer_Helper.ViewModel
         #endregion Member Variables
 
         #region View Properties
+        /// <summary>
+        /// MainWindow에 렌더링해줄 Component
+        /// </summary>
         private UserControl? _viewComponent;
         public UserControl? ViewComponent
         {
@@ -32,6 +36,19 @@ namespace Developer_Helper.ViewModel
                 if (_viewComponent != value) _viewComponent = value; OnpropertyChanged("ViewComponent");                
             }
         }
+
+        /// <summary>
+        /// PopupWindow 렌더링해줄 Component
+        /// </summary>
+        private UserControl? _popupComponent;
+        public UserControl? PopupComponent
+        {
+            get { return _popupComponent; }
+            set
+            {
+                if (_popupComponent != value) _popupComponent = value; OnpropertyChanged("PopupComponent");
+            }
+        }        
         #endregion View Properties
 
         #region Model Properties
@@ -43,6 +60,11 @@ namespace Developer_Helper.ViewModel
         #endregion Constructors
 
         #region Command
+        /// <summary>
+        /// author : yuminhio
+        /// date   : 2023-12-16
+        /// description : menuItem 선택 시 렌더링 되는 userControl을 변경하는 Command
+        /// </summary>
         private ICommand _selectedMenuItem;
         public ICommand SelectedMenuItem
         {
@@ -57,9 +79,26 @@ namespace Developer_Helper.ViewModel
 
         }
 
+        private ICommand _selectPopupMenuItem;
+        public ICommand SelectPopupMenuItem
+        {
+            get
+            {
+                if (_selectPopupMenuItem == null)
+                    _selectPopupMenuItem = new RelayCommand(prop => CallPopupMenu(prop));
+                
+                return _selectPopupMenuItem;
+            }                        
+        }
+
         #endregion Command
 
-        #region Method                
+        #region Method
+        /// <summary>
+        /// author : yuminhio
+        /// date   : 2023-12-16
+        /// description : menuItem클릭시 Tag 값을 파싱해서 파라미터로 넘겨준다.
+        /// </summary>
         private void SetVisibleUserControl(object p)
         {
             if (p == null) return;            
@@ -70,6 +109,11 @@ namespace Developer_Helper.ViewModel
 
         }
 
+        /// <summary>
+        /// author : yuminhio
+        /// date   : 2023-12-16
+        /// description : 파라미터로 받은 값으로 case별 UserControl을 Return한다.
+        /// </summary>
         private UserControl GetUserControl(string SelectedMenu)
         {
             UserControl? userControl = null;
@@ -81,15 +125,24 @@ namespace Developer_Helper.ViewModel
                     break;
                 case "SQL":
                     userControl = new ExcuteSQLControl();
-                    break;
+                    break;                
                 default:                    
                     break;
             }
 
             return userControl;
-
-
         }
+
+        private void CallPopupMenu(object p)
+        {
+            if (p == null) return;
+
+            string selectedMenu = ((System.Windows.FrameworkElement)p).Tag.ToString();
+            
+            SetDBConnetion_View pop = new SetDBConnetion_View();
+            pop.ShowDialog();
+        }                        
+
         #endregion Method
     }
 }
